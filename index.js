87623e8202983c09a3a475274e64031136c37fdd
ZAP1KAS <132051135+ZAP1KAS@users.noreply.github.com>
2025-06-27 13:21:04 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+const express = require('express');
+const fs = require('fs').promises;
+const app = express();
 
+// Эндпоинт для получения celka.lua
+app.get('/get-celka', async (req, res) => {
+    try {
+        const celkaLua = await fs.readFile('celka.lua', 'utf8');
+        res.status(200).send(celkaLua);
+    } catch (error) {
+        console.error('Ошибка:', error);
+        res.status(500).json({ message: 'Ошибка при загрузке celka.lua' });
+    }
+});
+
+// Запуск сервера
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Сервер запущен на порту ${PORT}`);
+});
